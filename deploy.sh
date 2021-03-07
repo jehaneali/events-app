8c46242b504466ee2ff1b4c1941e69f86ae60fb4
gigiali <ali.je@husky.neu.edu>
2021-03-06 23:45:07 -0500

    changed db url to events_app

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export MIX_ENV=prod
 export PORT=4797
 export NODEBIN=`pwd`/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
-export DATABSE_URL=ecto://events:events@localhost/events_app_prod
+export DATABSE_URL=ecto://events_app:events@localhost/events_app_prod
 
 echo "Building..."
 
