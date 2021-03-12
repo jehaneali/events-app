7e958b53eaaff5c478ceee50ac46cd0c6904ccd2
Jehane Ali <51268667+jehaneali@users.noreply.github.com>
2021-03-12 18:50:26 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,14 @@ export MIX_ENV=prod
 export PORT=4797
 export NODEBIN=`pwd`/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
-# TODO uncomment 
-#export DATABASE_URL=ecto://events:bad@localhost/events_app_prod
+export DATABASE_URL=ecto://events:bad@localhost/events_app_prod
 
 echo "Building..."
 
 mix deps.get --only prod
 mix compile
 export SECRET_KEY_BASE=E8L3Bzz6N+p5bi3wU8u5YILQT+V+Jw0ZaJyPDTwrK4ir9pAZ455QDFbFHR9f1end
-# TODO uncomment 
-#export DATABASE_URL=ecto://events:events@localhost/events_app_prod
+export DATABASE_URL=ecto://events:events@localhost/events_app_prod
 mix ecto.migrate
 (cd assets && npm install)
 (cd assets && webpack --mode production)
