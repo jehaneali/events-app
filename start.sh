10756d38909bf7a64c3c33f88538d75ae9450303
gigiali <ali.je@husky.neu.edu>
2021-03-13 05:34:53 -0500

    changed start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ export MIX_ENV=prod
 export PORT=4797
 export NODEBIN='pwd'/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
-# TODO uncomment 
-#export DATABASE_URL=ecto://events:events@localhost/events_app_prod
+export DATABASE_URL=ecto://events:events@localhost/events_app_prod
+# TODO change this
 _build/prod/rel/events_app/bin/events_app start
